798dd01a5d25f195f833e77538c1504b84c12984
sponticelli <sandro.ponticelli@gmail.com>
2020-07-13 16:32:54 +0200

    refactored division in a class

--- js/player.js
+++ js/player.js
@@ -1,18 +1,18 @@
 import { NumericValue } from "./numericValue.js";
-import { Division_Calc } from "./division.js";
+import { Division } from "./division.js";
 
 export class Player {
   constructor(id, coins, tokens) {
     this.ID = id;
     this.Coins = new NumericValue(coins);
     this.Tokens = new NumericValue(tokens);
-    this.Division = new NumericValue(Division_Calc(coins));
+    this.Division = new NumericValue(Division.calc(coins));
     this.MatchWon = new NumericValue(0);
     this.MatchLost = new NumericValue(0);
   }
 
   updateDivision() {
-    this.Division.Value = Division_Calc(this.Coins.Value);
+    this.Division.Value = Division.calc(this.Coins.Value);
   }
 
   delta() {
